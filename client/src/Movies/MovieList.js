a792e3a11a533ecda5a1af83924a79366aaf432c
Alan Lemasney <alanlemasney34@yahoo.com>
2019-06-05 00:11:26 -0600

    MVP complete, starting stretch

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
+import MovieInfo from './MovieInfo';
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -21,6 +23,7 @@ export default class MovieList extends Component {
   }
 
   render() {
+  
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
@@ -32,23 +35,10 @@ export default class MovieList extends Component {
 }
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+  return (
+    <Link to={`/movies/${movie.id}`}> 
+      <MovieInfo movie={movie} />
+    </Link>
   );
 }
